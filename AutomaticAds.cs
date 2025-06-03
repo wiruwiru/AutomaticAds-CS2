d8f437f652cc73172b2d46f0f6bfb7516cce6ce0
WiruWiru <61034981+wiruwiru@users.noreply.github.com>
2025-06-02 23:02:03 -0300

    fix: update plugin versión

--- AutomaticAds.cs
+++ AutomaticAds.cs
@@ -15,7 +15,7 @@ namespace AutomaticAds;
 public class AutomaticAdsBase : BasePlugin, IPluginConfig<BaseConfigs>
 {
     public override string ModuleName => "AutomaticAds";
-    public override string ModuleVersion => "1.2.3";
+    public override string ModuleVersion => "1.2.4";
     public override string ModuleAuthor => "luca.uy";
     public override string ModuleDescription => "Send automatic messages to the chat and play a sound alert for users to see the message.";
 
